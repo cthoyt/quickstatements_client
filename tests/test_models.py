5bc20d5d5765aff888cdf70f297fcb77e56e148e
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-02-25 20:14:55 +0100

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,11 +23,12 @@ class TestQuickStatements(unittest.TestCase):
     """Tests for quickstatements."""
 
     def test_prepare_date(self):
+        """Test the date cleaning function."""
         sample_orcid_data = json.loads(SAMPLE_ORCID_PATH.read_text())
         education_data = sample_orcid_data["activities-summary"]["educations"]["education-summary"]
 
-        test_start_date, start_date_precision = get_date(education_data[1])
-        test_end_date, end_date_precision = get_date(education_data[1], start_or_end="end")
+        test_start_date, start_date_precision = _get_date(education_data[1])
+        test_end_date, end_date_precision = _get_date(education_data[1], start_or_end="end")
 
         self.assertEqual(
             "+2015-08-00T00:00:00Z/10",
@@ -40,9 +41,9 @@ class TestQuickStatements(unittest.TestCase):
     def test_quickstatements(self):
         """Test quick statements."""
         subject_qid = "Q47475003"  # Charles Tapley Hoyt
-        subject_orcid = "0000-0003-4423-4370"
+        # subject_orcid = "0000-0003-4423-4370"
         reference_url_qualifier = TextQualifier(
-            predicate="S854", target=f"https://orcid.org/0000-0003-4423-4370"
+            predicate="S854", target="https://orcid.org/0000-0003-4423-4370"
         )
         start_date = datetime.datetime(year=2021, day=15, month=2)
         start_date_qualifier = DateQualifier.start_time(start_date)
@@ -54,7 +55,8 @@ class TestQuickStatements(unittest.TestCase):
             qualifiers=[reference_url_qualifier, start_date_qualifier, position_held_qualifier],
         )
         self.assertEqual(
-            'Q47475003|P108|Q49121|S854|"https://orcid.org/0000-0003-4423-4370"|P580|+2021-02-15T00:00:00Z/11|P39|Q1706722',
+            'Q47475003|P108|Q49121|S854|"https://orcid.org/0000-0003-4423-4370"|'
+            "P580|+2021-02-15T00:00:00Z/11|P39|Q1706722",
             employment_line.get_line(),
         )
 
@@ -73,7 +75,8 @@ class TestQuickStatements(unittest.TestCase):
             ],
         )
         self.assertEqual(
-            'Q47475003|P108|Q49121|S854|"https://orcid.org/0000-0003-4423-4370"|P580|+2021-02-00T00:00:00Z/10|P39|Q1706722',
+            'Q47475003|P108|Q49121|S854|"https://orcid.org/0000-0003-4423-4370"|P580|'
+            "+2021-02-00T00:00:00Z/10|P39|Q1706722",
             employment_line_less_precise.get_line(),
         )
 
@@ -85,9 +88,10 @@ class TestQuickStatements(unittest.TestCase):
         self.assertEqual('Q47475003|P1449|"Charlie"', nickname_line.get_line())
 
 
-def get_date(
+def _get_date(
     entry, start_or_end="start"
 ) -> Union[Tuple[datetime.datetime, int], Tuple[None, None]]:
+    """Get a date out of part of an ORCID record."""
     date = entry.get(f"{start_or_end}-date")
     if date is None:
         return None, None
