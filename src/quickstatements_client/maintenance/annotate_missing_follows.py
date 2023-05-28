106722e223d381e0f1ff59c3bc9eb4feef5cc959
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-05-28 14:30:45 +0200

    Add preprint/postprint follows maintenance script

---
+++ src/quickstatements_client/maintenance/annotate_missing_follows.py
@@ -0,0 +1,43 @@
+"""A maitenance script for adding missing "follows" relations to preprint/postprints."""
+
+from quickstatements_client.sources.utils import query_wikidata
+from quickstatements_client import EntityLine, QuickStatementsClient
+import click
+
+#: A SPARQL query that identifies preprints that are annotated
+#: with the "followed by" relationship to a postprint, but whose
+#: postprint doesn't have the "follows" relationship pointing
+#: backwards
+SPARQL = """\
+SELECT ?preprint ?postprint 
+WHERE {
+  ?preprint wdt:P31 wd:Q580922 .
+  ?preprint wdt:P156 ?postprint .
+  FILTER NOT EXISTS {
+    ?postprint wdt:P155 ?preprint .
+  }
+}
+"""
+
+
+@click.command()
+@click.option("--non-interactive", is_flag=True)
+def main(non_interactive: bool):
+    """Add missing "follows" relationships for preprint/postprint relations."""
+    lines = [
+        EntityLine(
+            subject=record["postprint"],
+            predicate="P155",
+            target=record["preprint"],
+        )
+        for record in query_wikidata(SPARQL)
+    ]
+    client = QuickStatementsClient()
+    if non_interactive:
+        client.post(lines, batch_name="Preprint Maitenance")
+    else:
+        client.open_new_tab(lines)
+
+
+if __name__ == "__main__":
+    main()
